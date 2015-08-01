7baa70f4938c4885d6d37cfcb70a22301bd69422
sparsis <soa5275@caelum-126-04>
2015-08-01 15:02:07 -0300

    arquivos dos cursos de EJB, JSF e JPA

---
+++ arquivos/26/dao-modelo/src/br/com/caelum/notasfiscais/util/ComponentResolver.java
@@ -0,0 +1,31 @@
+package br.com.caelum.notasfiscais.util;
+
+public class ComponentResolver {
+
+    public UIComponent findComponent(UIComponent component, String id) {
+
+        if (component.getId().equals(id)) {
+            return component;
+        }
+
+        UIComponent bingo = null;
+
+        for (UIComponent filho : component.getChildren()) {
+            bingo = this.findComponent(filho, id);
+            if (bingo != null)
+                return bingo;
+        }
+
+        return null;
+
+
+    }
+
+    public String getSubmittedValue(String idComponente) {
+        UIViewRoot arvore = FacesContext.getCurrentInstance().getViewRoot();
+        UIComponent componente = this.findComponent(arvore, idComponente);
+        UIInput input = (UIInput) componente;
+        return (String) input.getSubmittedValue();
+    }
+
+}
